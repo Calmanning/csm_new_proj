38b905fcebe5db427073768fae515afd86c31bcc
That Guy Cal <calmanning@gmail.com>
2021-04-07 13:14:32 -0700

    update to the homepage directory. Added SASS to the project.

--- src/App.js
+++ src/App.js
@@ -1,51 +1,19 @@
 import './App.css';
-import { Component } from 'react';
-import Cards from "./Components/Cards/"
-import SearchBox from "./Components/SearchBox/"
+// import Cards from "./Components/Cards/"
+// import SearchBox from "./Components/SearchBox/"
+import HomePage from "./Components/HomePage/"
 
 import React from 'react';
 
-class App extends Component{
-  constructor() {
-    super();
-
-    this.state = {
-      monsters : [],
-      searchField: ""
-    };
-
-  }
-
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then(response => response.json() )
-    .then(users => this.setState({ monsters: users }))
-  }
-
-  
-  
-  handleChange = (e) => {
-    this.setState({ searchField: e.target.value })
-  }
-
-  render(){
-
-      const { monsters, searchField } = this.state;
-      const filteredMonsters = monsters.filter(monster => 
-        monster.name.toLowerCase().includes(searchField.toLowerCase())
-        )
+function App() {
 
     return (
-      <div className = "App">
       <div>
-      word
+      <HomePage />
       </div>
+
       
-        <SearchBox placeholder="search for a thing" handleChange={this.handleChange} />
-      <Cards monsters={filteredMonsters} />      
-      </div>
   );
-    }
 }
 
 export default App;

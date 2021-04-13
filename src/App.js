896fc9b6c48ca62170a28d41001a1fabbe1db18a
That Guy Cal <calmanning@gmail.com>
2021-04-13 15:30:35 -0700

    added firebase authentic, and user account-uncoupling from application.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 // import Cards from "./Components/Cards/"
 // import SearchBox from "./Components/SearchBox/"
+import { auth } from "./firebase/firebase.utils"
 import HomePage from "./Pages/HomePage/";
 import ShopPage from "./Pages/ShopPage/";
 import SignInAndSignUpPage from "./Pages/SignInAndSignUpPage/";
@@ -9,20 +10,43 @@ import { Route, Switch } from "react-router-dom";
 
 import React from 'react';
 
-function App() {
+class App extends React.Component {
+ constructor() {
+   super();
+
+   this.state = {
+     currentUser: null
+   }
+ }
+
+unsubscribeFromAuth = null
+
+componentDidMount() {
+  this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+    this.setState({ currentUser: user })
+    console.log(user);
+  })
+}
+
+componentWillUnmount() {
+  this.unsubscribeFromAuth();
+}
+
+  render() {
 
     return (
       <div>
         <Header />
-      <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/shop" component={ShopPage} />
-      <Route path="/signin" component={SignInAndSignUpPage} />
-      </Switch>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUpPage} />
+        </Switch>
       </div>
 
-      
-  );
+
+    );
+  }
 }
 
 export default App;

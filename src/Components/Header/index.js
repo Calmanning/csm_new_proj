13e923f4a560b3535c6f4178d6e5f7379fac9164
That Guy Cal <calmanning@gmail.com>
2021-04-26 11:08:25 -0700

    Bringing Redux and associated packages into project. Added recuders. Have functioning user reducer.

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom"
-import "./Header.styles.scss"
+import { connect } from  "react-redux"
+
 import { ReactComponent as Logo } from "../../Assets/crown.svg"
 
 import { auth } from '../../firebase/firebase.utils'
+
+import "./Header.styles.scss"
 
 const Header = ({ currentUser }) => (
   <div className="header">
@@ -15,22 +18,27 @@ const Header = ({ currentUser }) => (
         SHOP
       </Link>
       <Link className="option" to="/shop">
-      CONTACT      
+        CONTACT
       </Link>
       {
-        currentUser ? 
-        <div className="option" 
-        onClick={() => auth.signOut()}> 
-        Sign Out 
-        </div>
-        :
-        <Link className="option" to="/signin">
-          SIGN IN
-        </Link>
-      }
+        currentUser ? (
+          <div className="option"
+            onClick={() => auth.signOut()}>
+            Sign Out
+          </div>
+        ) : (
+          <Link className="option" to="/signin">
+            SIGN IN
+          </Link>
+
+        )}
+      
     </div>
-  
   </div>
 )
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+ })
+
+export default connect(mapStateToProps)(Header);

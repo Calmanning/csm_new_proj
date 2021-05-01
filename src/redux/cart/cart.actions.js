9cb82ed06d2bb70140a7c294c4a4577298866593
That Guy Cal <calmanning@gmail.com>
2021-04-30 17:54:43 -0700

    added remove/add item and remove item functionality to the checkout page.

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -8,3 +8,13 @@ export const addItem = item => ({
   type: CartActionTypes.ADD_ITEM,
   payload: item
 });
+
+export const removeItemInCart = item => ({
+  type: CartActionTypes.REMOVE_ITEM_IN_CART,
+  payload: item
+})
+
+export const removeItem = item => ({
+  type: CartActionTypes.REMOVE_ITEM,
+  payload: item
+})

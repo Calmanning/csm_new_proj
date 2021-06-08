59f84ea2dd2ffa16b03de918092e51e4f39787a6
That Guy Cal <calmanning@gmail.com>
2021-06-07 19:54:41 -0700

    deleted homepage scss file. created new jsx style file for Header component.

--- src/Pages/HomePage/index.js
+++ src/Pages/HomePage/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import Directory from "../../Components/Directory/"
 
-import "./HomePage.styles.scss"
-
 import { HomePageContainer } from "./Homepage.styles"
 
 const HomePage = () => (
